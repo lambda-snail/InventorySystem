5890430e6edbede8ec38f6090d540ace790da5e3
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2024-06-09 18:52:19 +0200

    add clarifying comment

--- Source/LambdaSnailInventorySystem/Public/Inventory/Network/SubobjectReplicator.h
+++ Source/LambdaSnailInventorySystem/Public/Inventory/Network/SubobjectReplicator.h
@@ -11,6 +11,11 @@ class USubobjectReplicator : public UInterface
 	GENERATED_BODY()
 };
 
+/**
+ * Allows subobjects to register their own subobjects for replication with a parent. This allows a parent actor or component
+ * to be responsible for replicating more complicated structures, without having full knowledge of ho that structure works
+ * or without the need for detailed bookkeeping logic in the wrong place.
+ */
 class LAMBDASNAILINVENTORYSYSTEM_API ISubobjectReplicator
 {
 	GENERATED_BODY()
